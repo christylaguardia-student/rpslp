9957597fe2ec33656a2ab88ec22827a3a4d985f3
christylaguardia <christinelaguardia@gmail.com>
2018-03-08 01:45:03 -0800

    major components and styling

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Game from './game/Game';
+import Footer from './Footer';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Game />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/game/Play.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { rules } from './Rules';
+
+const Play = ({ handleClick }) => (
+  <div className="container is-vcentered">
+    <div className="level">
+      {Object.keys(rules).map((name, index) => {
+        return (
+          <div key={index} className="level-item">
+            <div>
+              <p className="iconButton box" onClick={() => handleClick(name)}>
+                <span className="icon is-large">
+                  <i className={`far fa-hand-${name} fa-3x`}></i>
+                </span>
+              </p>
+              <p className="heading">{name}</p>
+            </div>
+          </div>)
+      })}
+    </div>
+  </div>
+);
+
+export default Play;
